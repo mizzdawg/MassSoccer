6c175f898d1b46a04962795657f80f84690375f1
Max Carty <mcarty79@gmail.com>
2025-03-17 17:12:09 -0400

    Update left and right mobile nav deisgn and animations, add desktop nav layout, add desktop layout for homepage, add mobile and desktop layouts for divsions page

--- othsl-mock.js
+++ othsl-mock.js
@@ -1,24 +1,57 @@
 // Select all icons in the top nav and nav menus
-const navIcons = document.querySelectorAll('.nav-icon')
-const navMenus = document.querySelectorAll('.nav-pop-menu-container')
+// const navIcons = document.querySelectorAll('.nav-icon')
+// const navMenus = document.querySelectorAll('.nav-pop-menu-container')
+// const menuOverlays = document.querySelectorAll('.nav-pop-menu-overlay')
 
-// Add event listeners to each nav icon
-navIcons.forEach((icon, index) => {
-    icon.addEventListener('click', () => {
-        // Check if connected menu is active
-        if (navMenus[index].classList.contains('menu-active')) {
-            // Remove 'menu-active' class from active menu
-            navMenus[index].classList.remove('menu-active')
-        } else {
-            // Remove 'menu-active' class from all menus
-            navMenus.forEach(menu => {
-                menu.classList.remove('menu-active')
-            })
-            // Add 'menu-active' class to correct menu
-            navMenus[index].classList.add('menu-active')
-        }
-    })
+// // Add event listeners to each nav icon
+// navIcons.forEach((icon, index) => {
+//     icon.addEventListener('click', () => {
+//         const activeMenu = navMenus[index];
+
+//         // Check if menu is active
+//         if (activeMenu.classList.contains('menu-active')) {
+//             // Remove 'menu-active' class from active menu
+//             activeMenu.classList.remove('menu-active')
+//         } else {
+//             // Remove 'menu-active' class from all menus
+//             navMenus.forEach(menu => { menu.classList.remove('menu-active') })
+//             // Add 'menu-active' class to correct menu
+//             activeMenu.classList.add('menu-active')
+//         }
+//     })
+// })
+
+const navHamburger = document.querySelector('.ham-icon')
+const accountIcon = document.querySelector('.acct-icon')
+const leftNav = document.querySelector('.left-nav-pop')
+const rightNav = document.querySelector('.right-nav-pop')
+const leftNavX = document.querySelector('.nav-menu-x-icon')
+const rightNavX = document.querySelector('.acct-menu-x-icon')
+const navOverlay = document.querySelector('.nav-pop-menu-overlay')
+
+function toggleNav(nav) {
+    // toggle menu-active class
+    nav.classList.toggle('menu-active')
+    navOverlay.classList.toggle('overlay-active')
+}
+
+navHamburger.addEventListener('click', () => {
+    toggleNav(leftNav)
 })
+
+accountIcon.addEventListener('click', () => {
+    toggleNav(rightNav)
+})
+leftNavX.addEventListener('click', () => {
+    toggleNav(leftNav)
+})
+rightNavX.addEventListener('click', () => {
+    toggleNav(rightNav)
+})
+
+
+
+
 
 // Select all mobile top nav flyout headers, header chevron icons and each header's list of links
 const mobileNavHeaders = document.querySelectorAll('.nav-menu-pop-header')
